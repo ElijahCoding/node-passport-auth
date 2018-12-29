b471b3ca481bad55dd511c2df0b662e8e72309f2
LittleElijah <cuiyeqing960904@gmail.com>
2018-12-30 01:55:23 +0300

    stupid validation

--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,33 @@ const router = express.Router()
 router.get('/login', (req, res) => res.render('login'))
 router.get('/register', (req, res) => res.render('register'))
 
+router.post('/register', (req, res) => {
+    const { name, email, password, password2 } = req.body
+    let errors = []
+
+    if (!name || !email || !password || !password2) {
+        errors.push({ msg: 'Please fill in all fields' })
+    }
+
+    if (password !== password2) {
+        errors.push({ msg: 'Password does not match' })
+    }
+
+    if (password.length < 6) {
+        errors.push({ msg: 'Password shoud be at least 6 characters' })
+    }
+
+    if (errors.length > 0) {
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        })
+    } else {
+        res.send('pass')
+    }
+})
 
 module.exports = router
