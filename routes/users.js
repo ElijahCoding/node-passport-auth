b77167185eba358ec95b3e8a27eed3b5bfd160ca
LittleElijah <cuiyeqing960904@gmail.com>
2018-12-30 13:01:04 +0300

    register a user to db

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
+
+const User = require('../models/User')
 
 router.get('/login', (req, res) => res.render('login'))
 router.get('/register', (req, res) => res.render('register'))
@@ -29,7 +32,41 @@ router.post('/register', (req, res) => {
             password2
         })
     } else {
-        res.send('pass')
+        User.findOne({ email: email })
+            .then(user => {
+                if (user) {
+                    errors.push({
+                        msg: 'Email is already registered'
+                    })
+                    res.render('register', {
+                        errors,
+                        name,
+                        email,
+                        password,
+                        password2
+                    })
+                } else {
+                    const newUser = new User({
+                        name,
+                        email,
+                        password
+                    })
+
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if (err) throw err
+
+                            newUser.password = hash
+
+                            newUser.save()
+                                   .then(user => {
+                                       res.redirect('/users/login')
+                                   })
+                                   .catch(err => console.log(err))
+                        })
+                    })
+                }
+            })
     }
 })
 
